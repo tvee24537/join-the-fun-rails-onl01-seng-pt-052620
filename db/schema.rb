997cf1d61dd84f87975ebf5360fda7e04a91129b
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-12 22:04:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   create_table "rides", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
