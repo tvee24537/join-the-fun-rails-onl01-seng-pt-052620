654fd838c0d399bee8cf46317a29d337b09ad965
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-12 21:55:14 +0000

    Automatically backed up by Learn

--- db/migrate/001_add_ids_to_ride.rb
+++ db/migrate/001_add_ids_to_ride.rb
@@ -1,4 +1,4 @@
-class AddIdsToRides < ActiveRecord::Migration[5.0]
+class AddIdsToRides < ActiveRecord::Migration
   def change
     add_column :rides, :passenger_id, :integer
     add_column :rides, :taxi_id, :integer
